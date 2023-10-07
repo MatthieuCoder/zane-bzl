0d3d9692dfbe563e24a33c95c6c0b22281ae8663
Matthieu Pignolet <matthieu@matthieu-dev.xyz>
2023-10-07 19:14:52 +0400

    add frontend

---
+++ frontend/src/reportWebVitals.ts
@@ -0,0 +1,9 @@
+const reportWebVitals = (onPerfEntry) => {
+    if (onPerfEntry && onPerfEntry instanceof Function) {
+      import('web-vitals').then(({ onCLS, }) => {
+
+      });
+    }
+  };
+  
+  export default reportWebVitals;
